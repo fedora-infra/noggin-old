d292a8f4c6e763e89481b3ee8a5dc7c2318107ef
Patrick Uiterwijk <patrick@puiterwijk.org>
2018-11-13 08:15:15 +0100

    Add OIDC registration
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- CAIAPI/CAIAPI/default_config.py
+++ CAIAPI/CAIAPI/default_config.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
+
 # Secret key used to generate the CRSF token, thus very private!
 SECRET_KEY = 'CHANGEME'
 
 # Some OpenID Connect defaults
 OIDC_RESOURCE_SERVER_ONLY = True
 OIDC_INTROSPECTION_AUTH_METHOD = 'client_secret_basic'
+OIDC_CLIENT_SECRETS = os.getenv("OIDC_CLIENT_SECRETS")
